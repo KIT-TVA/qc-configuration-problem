e324e40fb58800df559e770a15d61aa0a144a411
Fabian Brenneisen <fabian.brenneisen@student.kit.edu>
2023-07-21 18:03:34 +0200

    Fix number of qubits being hardcoded and some cleanup

--- configproblem/qaoa_quantum_deflation.py
+++ configproblem/qaoa_quantum_deflation.py
@@ -9,13 +9,14 @@ valid_configs = ["101010", "101001", "101011", "100110", "100101", "100111", "00
                  "000111", "111010", "111001", "111011", "110110", "110101", "110111"]
 
 
-def convert_quso_matrix_to_numpy_ndarray(quso_matrix: QUSOMatrix) -> np.ndarray:
+def convert_quso_matrix_to_numpy_ndarray(quso_matrix: QUSOMatrix, nqubits: int) -> np.ndarray:
     """
         Converts a QUSOMatrix to a numpy ndarray.
 
         :param quso_matrix: the QUSOMatrix to convert
+        :param nqubits: the number of qubits
     """
-    matrix = np.zeros((6, 6))
+    matrix = np.zeros((nqubits, nqubits))
     for key, value in quso_matrix.items():
         acting_qubits = len(key)
         if acting_qubits == 0:
@@ -50,19 +51,6 @@ def convert_numpy_ndarray_to_quso_matrix(matrix: np.ndarray) -> QUSOMatrix:
     return quso_matrix
 
 
-def get_deflation_quso_matrix(config_str: str, deflation_factor: float) -> QUSOMatrix:
-    """
-        Calculated the deflation matrix as DictArithmetic for a given config and deflation factor.
-
-        :param config_str: the config to calculate the deflation matrix from
-        :param deflation_factor: the deflation factor to multiply the matrix with
-    """
-    config_array = np.array([[0 if s == "0" else 1 for s in config_str]])
-    deflation_matrix = np.matmul(config_array.transpose(), config_array)
-
-    return convert_numpy_ndarray_to_quso_matrix(deflation_matrix * deflation_factor)
-
-
 def deflate_config(hamiltonian: QUSOMatrix, config_str: str, deflation_factor: float) -> QUSOMatrix:
     """
         Deflates the hamiltonian with the given config.
@@ -71,30 +59,36 @@ def deflate_config(hamiltonian: QUSOMatrix, config_str: str, deflation_factor: f
         :param config_str: the config to deflate the hamiltonian with
         :param deflation_factor: the deflation factor to use
     """
-    return hamiltonian + get_deflation_quso_matrix(config_str, deflation_factor)
+    config_array = np.array([[0 if s == "0" else 1 for s in config_str]])
+    deflation_matrix = np.matmul(config_array.transpose(), config_array)
+
+    return hamiltonian + convert_numpy_ndarray_to_quso_matrix(deflation_matrix * deflation_factor)
 
 
-def get_probabilities_dict(hamiltonian: QUSOMatrix) -> dict[str, float]:
+def get_probabilities_dict(hamiltonian: QUSOMatrix, nqubits: int) -> dict[str, float]:
     """
         Calculates the probabilities for all configs of a given hamiltonian.
 
         :param hamiltonian: the hamiltonian of the optimization problem
+        :param nqubits: the number of qubits
     """
     probabilities, _ = apply_qaoa_statevector(hamiltonian, print_res=False)
     probabilities_dict = {}
-    for j in range(0, 2 ** 6):
-        probabilities_dict[(np.binary_repr(j, width=6))] = round(probabilities[j], 4)
+    for j in range(0, 2 ** nqubits):
+        probabilities_dict[(np.binary_repr(j, width=nqubits))] = round(probabilities[j], 4)
     return probabilities_dict
 
 
-def strategy_projection_deflation(hamiltonian: QUSOMatrix, output_list_size: int, deflation_factor_start_value: float)\
-        -> list[str]:
+def strategy_projection_deflation(hamiltonian: QUSOMatrix, nqubits: int, output_list_size: int,
+                                  deflation_factor_start_value: float, debug_output: bool = False) -> list[str]:
     """
         Calculates the output list for the given hamiltonian using the projection deflation strategy.
 
         :param hamiltonian: the hamiltonian of the optimization problem
+        :param nqubits: the number of qubits
         :param output_list_size: the size of the output list
         :param deflation_factor_start_value: the start value for the deflation factor
+        :param debug_output: whether to print debug output
     """
     current_hamiltonian = hamiltonian
     output_list = []
@@ -103,10 +97,10 @@ def strategy_projection_deflation(hamiltonian: QUSOMatrix, output_list_size: int
     deflation_factor_value = deflation_factor_start_value
     i = 0
     while i < output_list_size:
-        print(current_hamiltonian)
-        probabilities_dict = get_probabilities_dict(current_hamiltonian)
-
-        plot_counts_histogram(probabilities_dict, best_config, valid_configs)
+        probabilities_dict = get_probabilities_dict(current_hamiltonian, nqubits)
+        if debug_output:
+            print("Current hamiltonian: " + str(current_hamiltonian))
+            plot_counts_histogram(probabilities_dict, best_config, valid_configs)
 
         output_list.append(max(probabilities_dict, key=probabilities_dict.get))
         config_energies.append(compute_config_energy(hamiltonian, [-1 if s == "0" else 1 for s in output_list[-1]]))
@@ -118,21 +112,23 @@ def strategy_projection_deflation(hamiltonian: QUSOMatrix, output_list_size: int
             config_energies.clear()
             current_hamiltonian = hamiltonian
             i = 0
-            print("Restarting with deflation factor: " + str(deflation_factor_value))
+            if debug_output:
+                print("Restarting with deflation factor: " + str(deflation_factor_value))
             continue
 
         # Determine A_k+1
-        deflation_matrix = np.identity(6)
+        deflation_matrix = np.identity(nqubits)
         for config in output_list:
             config_array = np.array([[0 if s == "0" else 1 for s in config]])
-            config_matrix = np.identity(6) - np.matmul(config_array.transpose(), config_array)
+            config_matrix = np.identity(nqubits) - np.matmul(config_array.transpose(), config_array)
 
             deflation_matrix = np.matmul(deflation_matrix, config_matrix)
 
         # Determine H_k+1
-        hamiltonian_ndarray = convert_quso_matrix_to_numpy_ndarray(current_hamiltonian)
+        hamiltonian_ndarray = convert_quso_matrix_to_numpy_ndarray(current_hamiltonian, nqubits)
         new_hamiltonian_ndarray = np.matmul(deflation_matrix.transpose(),
-                                            np.matmul(hamiltonian_ndarray - deflation_factor_value * np.identity(6),
+                                            np.matmul(hamiltonian_ndarray
+                                                      - deflation_factor_value * np.identity(nqubits),
                                                       deflation_matrix))
         current_hamiltonian = convert_numpy_ndarray_to_quso_matrix(new_hamiltonian_ndarray)
         i += 1
@@ -140,14 +136,17 @@ def strategy_projection_deflation(hamiltonian: QUSOMatrix, output_list_size: int
     return output_list
 
 
-def strategy_variational_quantum_deflation(hamiltonian: QUSOMatrix, output_list_size: int,
-                                           deflation_factor_start_value: float) -> list[str]:
+def strategy_variational_quantum_deflation(hamiltonian: QUSOMatrix, nqubits:int, output_list_size: int,
+                                           deflation_factor_start_value: float, debug_output: bool = False)\
+        -> list[str]:
     """
         Calculates the output list for the given hamiltonian using the variational quantum deflation strategy.
 
         :param hamiltonian: the hamiltonian of the optimization problem
+        :param nqubits: the number of qubits
         :param output_list_size: the size of the output list
         :param deflation_factor_start_value: the start value for the deflation factor
+        :param debug_output: whether to print debug output
     """
     current_hamiltonian = hamiltonian
     output_list = []
@@ -155,11 +154,11 @@ def strategy_variational_quantum_deflation(hamiltonian: QUSOMatrix, output_list_
     deflation_factor_value = deflation_factor_start_value
     i = 0
     while i < output_list_size:
-        print("Current hamiltonian: " + str(current_hamiltonian))
         restart = False
-        probabilities_dict = get_probabilities_dict(current_hamiltonian)
-
-        plot_counts_histogram(probabilities_dict, best_config, valid_configs)
+        probabilities_dict = get_probabilities_dict(current_hamiltonian, nqubits)
+        if debug_output:
+            print("Current hamiltonian: " + str(current_hamiltonian))
+            plot_counts_histogram(probabilities_dict, best_config, valid_configs)
 
         current_config = max(probabilities_dict, key=probabilities_dict.get)
         current_config_energy = compute_config_energy(hamiltonian, [-1 if s == "0" else 1 for s in current_config])
@@ -172,7 +171,8 @@ def strategy_variational_quantum_deflation(hamiltonian: QUSOMatrix, output_list_
                 current_hamiltonian = hamiltonian
                 i = 0
                 restart = True
-                print("Restarting with deflation factor: " + str(deflation_factor_value))
+                if debug_output:
+                    print("Restarting with deflation factor: " + str(deflation_factor_value))
                 break
         if restart:
             continue
@@ -184,20 +184,24 @@ def strategy_variational_quantum_deflation(hamiltonian: QUSOMatrix, output_list_
     return output_list
 
 
-def config_prioritization(hamiltonian: QUSOMatrix, output_list_size: int, deflation_factor_start_value: float,
-                          strategy='vqd') -> list[str]:
+def config_prioritization(hamiltonian: QUSOMatrix, nqubits: int, output_list_size: int,
+                          deflation_factor_start_value: float, strategy='vqd', debug_output: bool = False) -> list[str]:
     """
         Calculates the output list for the given hamiltonian using the given strategy.
         Available strategies are: 'vqd' (variational quantum deflation) and 'pd' (projection deflation).
 
         :param hamiltonian: the hamiltonian of the optimization problem
+        :param nqubits: the number of qubits
         :param output_list_size: the size of the output list
         :param deflation_factor_start_value: the start value for the deflation factor
         :param strategy: the strategy to use
+        :param debug_output: whether to print debug output
     """
     if strategy == 'vqd':
-        return strategy_variational_quantum_deflation(hamiltonian, output_list_size, deflation_factor_start_value)
+        return strategy_variational_quantum_deflation(hamiltonian, nqubits, output_list_size,
+                                                      deflation_factor_start_value, debug_output=debug_output)
     elif strategy == 'pd':
-        return strategy_projection_deflation(hamiltonian, output_list_size, deflation_factor_start_value)
+        return strategy_projection_deflation(hamiltonian, nqubits, output_list_size, deflation_factor_start_value,
+                                             debug_output=debug_output)
     else:
         raise ValueError(f"Unsupported strategy: {strategy}")
