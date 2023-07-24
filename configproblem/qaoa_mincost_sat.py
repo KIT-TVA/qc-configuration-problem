d03802d91e88dc54bf0ae2776e613edd246c03ce
Fabian Brenneisen <brenneisen.fabian+github@gmail.com>
2023-07-24 09:54:16 +0200

    Config prioritization (#21)
    Co-authored-by: Fabian Brenneisen <fabian.brenneisen@student.kit.edu>

--- configproblem/qaoa_mincost_sat.py
+++ configproblem/qaoa_mincost_sat.py
@@ -1,13 +1,17 @@
 from qiskit import Aer, transpile, QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit.circuit import Parameter
 import matplotlib.pyplot as plt
+import numpy as np
+from qiskit_aer import StatevectorSimulator
 from scipy.optimize import minimize, basinhopping
+from qubovert.utils import DictArithmetic
 
 from pprint import pprint
 import math
 
 from fragments.quantum_states import superposition_circuit
-from util.hamiltonian_math import compute_hamiltonian_energy
+from util.hamiltonian_math import compute_hamiltonian_energy, compute_hamiltonian_energy_from_statevector
+
 
 def mixer_circuit(nqubits: int) -> QuantumCircuit:
     beta = Parameter("$\\beta$")
@@ -107,8 +111,9 @@ def get_expectation(hamiltonian, nqubits, nlayers, shots=128, amplitude_vector=N
 
 def apply_qaoa(hamiltonian, layers=60, n_features=6, shots=256, theta={"beta": 0.01, "gamma": -0.01}, warmstart_statevector=None, use_optimizer=True):
     """
-        Applies the QAOA Algorithm for the given problem hamiltonian in QUBO form.
-        
+        Applies the QAOA Algorithm for the given problem hamiltonian in QUSO form.
+
+        :param hamiltonian: the hamiltonian used for creating the quantum circuit and determining the expected config cost
         :param int layers: the hyperparameter p of QAOA defining how many cost-mixer-layers will be in the circuit
         :param int n_features: the number of independent variables in the input hamiltonian
         :param int shots: the number of shots used in a simulator run of the QAOA quantum circuit
@@ -128,3 +133,66 @@ def apply_qaoa(hamiltonian, layers=60, n_features=6, shots=256, theta={"beta": 0
     # run qaoa circuit with parameters in theta
     counts, qc = quantum(hamiltonian, n_features, layers, theta["beta"], theta["gamma"], shots, warmstart_statevector)
     return counts, qc
+
+
+def quantum_statevector(hamiltonian, nqubits, layers, beta_val, gamma_val, amplitude_vector=None):
+    qc, beta, gamma = qaoa_circuit(hamiltonian, nqubits, layers, amplitude_vector, measure=False)
+
+    # Set parameters for qc
+    qc = qc.bind_parameters({
+        beta: beta_val,
+        gamma: gamma_val
+    })
+
+    # run and measure qc
+    statevector_sim = StatevectorSimulator()
+    transpiled_qaoa = transpile(qc, statevector_sim)
+    result = statevector_sim.run(transpiled_qaoa).result()
+    probabilities = result.get_statevector().probabilities()
+
+    return probabilities, qc
+
+
+def get_expectation_statevector(hamiltonian, nqubits, nlayers, amplitude_vector=None, strategy='min'):
+    backend = StatevectorSimulator()
+
+    def execute_circ(theta):
+        qc, beta, gamma = qaoa_circuit(hamiltonian, nqubits, nlayers, amplitude_vector, measure=False)
+
+        # Set parameters for qc
+        qc = qc.bind_parameters({
+            beta: theta[0],
+            gamma: theta[1]
+        })
+
+        statevector = backend.run(qc).result().get_statevector()
+
+        return compute_hamiltonian_energy_from_statevector(hamiltonian, statevector, nqubits, strategy=strategy)
+
+    return execute_circ
+
+
+def apply_qaoa_statevector(hamiltonian, layers=60, n_features=6, theta={"beta": 0.01, "gamma": -0.01}, warmstart_statevector=None, use_optimizer=True, print_res=True):
+    """
+        Applies the QAOA Algorithm for the given hamiltonian in QUSO form.
+
+        :param hamiltonian: the hamiltonian used for creating the quantum circuit and determining the expected config cost
+        :param int layers: the hyperparameter p of QAOA defining how many cost-mixer-layers will be in the circuit
+        :param int n_features: the number of independent variables in the input hamiltonian
+        :param dict theta: dictionary with keys "beta" and "gamma" that parameterize the QAOA circuit, used as start value when optimizing
+        :param list warmstart_statevector: statevector to warmstart to, instead of creating an equal superposition
+        :param bool use_optimizer: indicates whether to optimize theta using classical optimizers
+        :param bool print_res: indicates whether the results of the optimization should be printed
+    """
+    # define expectation function for optimizers
+    expectation = get_expectation_statevector(hamiltonian, n_features, layers, warmstart_statevector)
+
+    # optimize beta and gamma
+    if use_optimizer:
+        res = minimize(expectation, [theta["beta"], theta["gamma"]], method='COBYLA', tol=1e-12)
+        if print_res:
+            print(res)
+        theta = {"beta": res.x[0], "gamma": res.x[1]}
+
+    probabilities, qc = quantum_statevector(hamiltonian, n_features, layers, theta["beta"], theta["gamma"], warmstart_statevector)
+    return probabilities, qc
