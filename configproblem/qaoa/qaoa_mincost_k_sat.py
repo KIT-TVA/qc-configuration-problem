9ec8503408a5bc5cdfbc657f0e4c3a7f74e7f33e
Fabian Brenneisen <fabian.brenneisen@student.kit.edu>
2023-07-26 13:14:27 +0200

    Add prioritization using HOBOs

--- configproblem/qaoa/qaoa_mincost_k_sat.py
+++ configproblem/qaoa/qaoa_mincost_k_sat.py
@@ -1,5 +1,8 @@
 from qiskit import QuantumCircuit
 from qiskit.circuit import Parameter
+from qubovert import boolean_var
+
+from configproblem.util.model_transformation import convert_clause_to_penalty
 import math
 
 from qubovert.utils import DictArithmetic
@@ -45,3 +48,13 @@ def problem_circuit(hamiltonian: DictArithmetic, nqubits: int) -> tuple[QuantumC
         qc_p.compose(k_rz_gate(qc_p, list(key), 2 * normalization * factor * gamma))
 
     return qc_p, gamma
+
+
+def exclude_config(hamiltonian: DictArithmetic, config: str, alpha_sat: int) -> DictArithmetic:
+    sat_exclusion_clause = []
+    for index, value in enumerate(config):
+        if value == '0':
+            sat_exclusion_clause.append((boolean_var(index), True))
+        else:
+            sat_exclusion_clause.append((boolean_var(index), False))
+    return hamiltonian + (alpha_sat * convert_clause_to_penalty(sat_exclusion_clause)[0][0].to_puso())
