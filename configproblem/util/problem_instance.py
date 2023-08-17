731947b302df1e66077c29e3dc2e91dd4feb8260
Fabian Brenneisen <fabian.brenneisen@student.kit.edu>
2023-08-17 17:47:06 +0200

    Refactor ProblemInstance class

--- configproblem/util/problem_instance.py
+++ configproblem/util/problem_instance.py
@@ -1,6 +1,6 @@
 import numpy as np
 from qubovert import boolean_var, PCBO
-from qubovert.utils import PUSOMatrix, DictArithmetic
+from qubovert.utils import DictArithmetic
 
 from configproblem.util.model_transformation import convert_to_penalty
 
@@ -9,49 +9,47 @@ class ProblemInstance:
     """ Represents a SAT instance with a cost associated with each feature"""
     sat_instance: list[list[tuple[boolean_var, bool]]]
     boolean_variables: list[boolean_var]
-    n_features: int
     feature_cost: list[int]
+    alpha_sat: float
     valid_configs: list[str] = None
     best_config: str = None
     sat_model: PCBO = None
     cost_model: PCBO = None
 
     def __init__(self, sat_instance: list[list[tuple[boolean_var, bool]]], boolean_variables: boolean_var,
-                 feature_cost: list[int]):
+                 feature_cost: list[int], alpha_sat: float):
         """
             :param sat_instance: list of clauses, each clause is a list of tuples (variable, is_not_negated)
             :param boolean_variables: ordered list of boolean variables
             :param feature_cost: list of costs for each feature
         """
-        # count number of distinct variables in sat_instance
         variables = []
         for clause in sat_instance:
             for variable, _ in clause:
                 if variable not in variables:
                     variables.append(variable)
 
-        n_features = len(variables)
+        if not set(variables).issubset(set(boolean_variables)):
+            raise ValueError("boolean_variables must contain all variables used in sat_instance")
 
-        if len(feature_cost) != n_features:
-            raise ValueError("feature_cost must have same length as number of features")
-        if len(boolean_variables) != n_features:
-            raise ValueError("boolean_variables must have same length as number of features")
+        if len(boolean_variables) != len(feature_cost):
+            raise ValueError("boolean_variables and feature_cost must have the same length")
 
         self.sat_instance = sat_instance
         self.boolean_variables = boolean_variables
-        self.n_features = n_features
         self.feature_cost = feature_cost
+        self.alpha_sat = alpha_sat
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"sat_instance: {self.sat_instance}\n" \
-               f"n_features: {self.n_features}\n" \
+               f"n_features: {len(self.boolean_variables)}\n" \
                f"feature_cost: {self.feature_cost}\n"
 
     def get_sat_instance(self) -> list[list[tuple[boolean_var, bool]]]:
         return self.sat_instance
 
     def get_num_features(self) -> int:
-        return self.n_features
+        return len(self.boolean_variables)
 
     def get_feature_cost(self) -> list[int]:
         return self.feature_cost
@@ -61,8 +59,8 @@ class ProblemInstance:
             return self.valid_configs
 
         valid_configs = []
-        for i in range(2 ** self.n_features):
-            config = np.binary_repr(i, width=self.n_features)
+        for i in range(2 ** len(self.boolean_variables)):
+            config = np.binary_repr(i, width=len(self.boolean_variables))
             if self.__check_config_validity(config):
                 valid_configs.append(config)
         self.valid_configs = valid_configs
@@ -103,7 +101,7 @@ class ProblemInstance:
             Returns the cost of a given config
         """
         cost = 0
-        for i in range(self.n_features):
+        for i in range(len(self.boolean_variables)):
             if config[-(i + 1)] == '1':
                 cost += self.feature_cost[i]
         return cost
@@ -118,13 +116,13 @@ class ProblemInstance:
         if self.cost_model is not None:
             return self.cost_model.to_puso()
         cost_model = PCBO()
-        for i in range(self.n_features):
+        for i in range(len(self.boolean_variables)):
             cost_model += self.feature_cost[i] * self.boolean_variables[i]
         self.cost_model = cost_model
         return self.cost_model.to_puso()
 
-    def get_puso_combined_hamiltonian(self, alpha_sat: int) -> DictArithmetic:
-        return alpha_sat * self.get_puso_sat_hamiltonian() + self.get_cost_hamiltonian()
+    def get_puso_combined_hamiltonian(self) -> DictArithmetic:
+        return self.alpha_sat * self.get_puso_sat_hamiltonian() + self.get_cost_hamiltonian()
 
     def get_quso_sat_hamiltonian(self) -> DictArithmetic:
         if self.sat_model is not None:
@@ -132,5 +130,5 @@ class ProblemInstance:
         self.sat_model = convert_to_penalty(self.sat_instance)
         return self.sat_model.to_quso()
 
-    def get_quso_combined_hamiltonian(self, alpha_sat: int) -> DictArithmetic:
-        return alpha_sat * self.get_quso_sat_hamiltonian() + self.get_cost_hamiltonian()
+    def get_quso_combined_hamiltonian(self) -> DictArithmetic:
+        return self.alpha_sat * self.get_quso_sat_hamiltonian() + self.get_cost_hamiltonian()
