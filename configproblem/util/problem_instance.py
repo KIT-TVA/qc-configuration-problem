ef4cf0b45675b10ee26f3be375af7cd4bcc040cd
Fabian Brenneisen <fabian.brenneisen@student.kit.edu>
2023-09-03 21:41:05 +0200

    Add different instance set generation methods

--- configproblem/util/problem_instance.py
+++ configproblem/util/problem_instance.py
@@ -5,17 +5,168 @@ from qubovert.utils import DictArithmetic
 from configproblem.util.model_transformation import convert_to_penalty
 
 
-def set_seed(seed: int):
+def generate_problem_instance_set(n_instances: int, n_features: int, min_n_clauses: int, max_n_clauses: int,
+                                  min_clause_length: int, max_clause_length: int, min_feature_cost: int,
+                                  max_feature_cost: int, alpha_sat: float, seed: int = 42,
+                                  generation_type: str = 'random', start_instance: 'ProblemInstance' = None) \
+        -> list['ProblemInstance']:
     """
-        Sets the seed for the random number generator
+        Generates a set of problem instances
 
+        :param n_instances: number of problem instances to generate
+        :param n_features: number of features
+        :param min_n_clauses: minimum number of clauses
+        :param max_n_clauses: maximum number of clauses
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+        :param alpha_sat: weight of the SAT part of the objective function,
+                          will be considered as the maximum weight if generation_type is 'alpha_sat'
         :param seed: seed for the random number generator
+        :param generation_type: type of generation, can be 'random', 'growing_num_clauses', 'append_clauses' or
+                                'alpha_sat'
+        :param start_instance: problem instance to use as a starting point for 'alpha_sat' generation type
     """
     np.random.seed(seed)
+    if generation_type == 'random':
+        return generate_problem_instance_set_random(n_instances, n_features, min_n_clauses, max_n_clauses,
+                                                    min_clause_length, max_clause_length, min_feature_cost,
+                                                    max_feature_cost, alpha_sat)
+    elif generation_type == 'growing_num_clauses':
+        return generate_problem_instance_set_growing_num_clauses(n_instances, n_features, min_n_clauses, max_n_clauses,
+                                                                 min_clause_length, max_clause_length,
+                                                                 min_feature_cost, max_feature_cost, alpha_sat)
+    elif generation_type == 'append_clauses':
+        return generate_problem_instance_set_append_clauses(n_instances, n_features, min_n_clauses, max_n_clauses,
+                                                            min_clause_length, max_clause_length, min_feature_cost,
+                                                            max_feature_cost, alpha_sat)
+    elif generation_type == 'alpha_sat':
+        return generate_problem_instance_set_alpha_sat(n_instances, n_features, min_n_clauses, max_n_clauses,
+                                                       min_clause_length, max_clause_length, min_feature_cost,
+                                                       max_feature_cost, alpha_sat, start_instance=start_instance)
+    else:
+        raise ValueError(f"generation_type {generation_type} is not supported")
+
+
+def generate_problem_instance_set_random(n_instances: int, n_features: int, min_n_clauses: int, max_n_clauses: int,
+                                         min_clause_length: int, max_clause_length: int, min_feature_cost: int,
+                                         max_feature_cost: int, alpha_sat: float) -> list['ProblemInstance']:
+    """
+        Generates a set of random problem instances
+
+        :param n_instances: number of problem instances to generate
+        :param n_features: number of features
+        :param min_n_clauses: minimum number of clauses
+        :param max_n_clauses: maximum number of clauses
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+        :param alpha_sat: weight of the SAT part of the objective function
+    """
+    problem_instances = []
+    for _ in range(n_instances):
+        problem_instances.append(generate_problem_instance(n_features, min_n_clauses, max_n_clauses, min_clause_length,
+                                                           max_clause_length, min_feature_cost, max_feature_cost,
+                                                           alpha_sat))
+    return problem_instances
+
+
+def generate_problem_instance_set_growing_num_clauses(n_instances: int, n_features: int, min_n_clauses: int,
+                                                      max_n_clauses: int, min_clause_length: int,
+                                                      max_clause_length: int, min_feature_cost: int,
+                                                      max_feature_cost: int, alpha_sat: float) \
+        -> list['ProblemInstance']:
+    """
+        Generates a set of problem instances with a growing number of clauses
+
+        :param n_instances: number of problem instances to generate
+        :param n_features: number of features
+        :param min_n_clauses: minimum number of clauses
+        :param max_n_clauses: maximum number of clauses
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+        :param alpha_sat: weight of the SAT part of the objective function
+    """
+    n_instances_per_n_clauses = int(n_instances / (max_n_clauses - min_n_clauses + 1))
+    problem_instances = []
+    for n_clauses in range(min_n_clauses, max_n_clauses + 1):
+        for _ in range(n_instances_per_n_clauses):
+            problem_instances.append(generate_problem_instance(n_features, n_clauses, n_clauses, min_clause_length,
+                                                               max_clause_length, min_feature_cost, max_feature_cost,
+                                                               alpha_sat))
+    return problem_instances
+
+
+def generate_problem_instance_set_append_clauses(n_instances: int, n_features: int, min_n_clauses: int,
+                                                 max_n_clauses: int, min_clause_length: int, max_clause_length: int,
+                                                 min_feature_cost: int, max_feature_cost: int, alpha_sat: float) \
+        -> list['ProblemInstance']:
+    """
+        Generates a set of problem instances by adding clauses to an existing problem instance
+
+        :param n_instances: number of problem instances to generate
+        :param n_features: number of features
+        :param min_n_clauses: minimum number of clauses
+        :param max_n_clauses: maximum number of clauses
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+        :param alpha_sat: weight of the SAT part of the objective function
+    """
+    n_start_instances = int(n_instances / (max_n_clauses - min_n_clauses + 1))
+    problem_instances = []
+    for _ in range(n_start_instances - 1):
+        problem_instances.append(generate_problem_instance(n_features, min_n_clauses, min_n_clauses, min_clause_length,
+                                                           max_clause_length, min_feature_cost, max_feature_cost,
+                                                           alpha_sat))
+        for _ in range(min_n_clauses + 1, max_n_clauses + 1):
+            problem_instances.append(get_problem_instance_with_added_clause(problem_instances[-1], n_features,
+                                                                            min_clause_length, max_clause_length,
+                                                                            min_feature_cost, max_feature_cost))
+    return problem_instances
+
+
+def generate_problem_instance_set_alpha_sat(n_instances: int, n_features: int, min_n_clauses: int,
+                                            max_n_clauses: int, min_clause_length: int, max_clause_length: int,
+                                            min_feature_cost: int, max_feature_cost: int, alpha_sat: float,
+                                            start_instance: 'ProblemInstance' = None) -> list['ProblemInstance']:
+    """
+        Generates a set of problem instances with a growing alpha_sat
+
+        :param n_instances: number of problem instances to generate
+        :param n_features: number of features
+        :param min_n_clauses: minimum number of clauses
+        :param max_n_clauses: maximum number of clauses
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+        :param alpha_sat: maximum weight of the SAT part of the objective function
+        :param start_instance: problem instance to use as a starting point
+    """
+    alpha_sat_step = alpha_sat / n_instances
+    if start_instance is not None:
+        problem_instances = [ProblemInstance(start_instance.get_sat_instance(), start_instance.get_boolean_variables(),
+                                             start_instance.get_feature_cost(), alpha_sat_step)]
+    else:
+        problem_instances = [generate_problem_instance(n_features, min_n_clauses, max_n_clauses, min_clause_length,
+                                                       max_clause_length, min_feature_cost, max_feature_cost,
+                                                       alpha_sat_step)]
+    for _ in range(n_instances - 1):
+        problem_instances.append(ProblemInstance(problem_instances[-1].get_sat_instance(),
+                                                 problem_instances[-1].get_boolean_variables(),
+                                                 problem_instances[-1].get_feature_cost(),
+                                                 problem_instances[-1].alpha_sat + alpha_sat_step))
+    return problem_instances
 
 
 def generate_problem_instance(n_features: int, min_n_clauses: int, max_n_clauses: int, min_clause_length: int,
-                              max_clause_length: int, min_feature_cost: int, max_feature_cost: int, alpha_sat: float)\
+                              max_clause_length: int, min_feature_cost: int, max_feature_cost: int, alpha_sat: float) \
         -> 'ProblemInstance':
     """
         Generates a random problem instance
@@ -29,12 +180,45 @@ def generate_problem_instance(n_features: int, min_n_clauses: int, max_n_clauses
         :param max_feature_cost: maximum cost of a feature
         :param alpha_sat: weight of the SAT part of the objective function
     """
-
     variables = [boolean_var(f"x{i}") for i in range(n_features)]
     feature_cost = list(np.random.randint(min_feature_cost, max_feature_cost + 1, size=n_features))
     sat_instance = generate_sat_instance(variables, min_n_clauses, max_n_clauses, min_clause_length, max_clause_length)
 
     return ProblemInstance(sat_instance, variables, feature_cost, alpha_sat)
+
+
+def get_problem_instance_with_added_clause(problem_instance: 'ProblemInstance', n_features: int, min_clause_length: int,
+                                           max_clause_length: int, min_feature_cost: int, max_feature_cost: int) \
+        -> 'ProblemInstance':
+    """
+        Generates a new problem instance by adding a clause to an existing problem instance
+
+        :param problem_instance: problem instance to which a clause will be added
+        :param n_features: number of features
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+        :param min_feature_cost: minimum cost of a feature
+        :param max_feature_cost: maximum cost of a feature
+    """
+    sat_instance = problem_instance.get_sat_instance().copy()
+    variables = problem_instance.boolean_variables.copy()
+    feature_cost = problem_instance.feature_cost.copy()
+
+    if n_features > len(variables):
+        variables_str = [list(var.keys())[0][0] for var in variables]
+        for i in range(n_features):
+            if variables_str.__contains__(f"x{i}"):
+                continue
+            else:
+                variables.append(boolean_var(f"x{i}"))
+                feature_cost.append(np.random.randint(min_feature_cost, max_feature_cost + 1))
+
+        # sort variables and feature_cost
+        variables = [x for _, x in sorted(zip(variables_str, variables))]
+        feature_cost = [x for _, x in sorted(zip(variables_str, feature_cost))]
+
+    sat_instance.append(generate_sat_clause(variables, min_clause_length, max_clause_length))
+    return ProblemInstance(sat_instance, variables, feature_cost, problem_instance.alpha_sat)
 
 
 def generate_sat_instance(variables: list[boolean_var], min_n_clauses: int, max_n_clauses: int, min_clause_length: int,
@@ -48,21 +232,32 @@ def generate_sat_instance(variables: list[boolean_var], min_n_clauses: int, max_
         :param min_clause_length: minimum number of variables in a clause
         :param max_clause_length: maximum number of variables in a clause
     """
-
     n_clauses = np.random.randint(min_n_clauses, max_n_clauses + 1)
     sat_instance = []
     for _ in range(n_clauses):
-        clause_length = np.random.randint(min_clause_length, max_clause_length + 1)
-        clause = []
-        available_variables = variables.copy()
-        for _ in range(clause_length):
-            variable = np.random.choice(available_variables)
-            is_not_negated = np.random.choice([True, False])
-            clause.append((variable, is_not_negated))
-            available_variables.remove(variable)
-        sat_instance.append(clause)
+        sat_instance.append(generate_sat_clause(variables, min_clause_length, max_clause_length))
 
     return sat_instance
+
+
+def generate_sat_clause(variables: list[boolean_var], min_clause_length: int, max_clause_length: int) \
+        -> list[tuple[boolean_var, bool]]:
+    """
+        Generates a random SAT clause with length between min_clause_length and max_clause_length
+
+        :param variables: list of boolean variables that can be used in the SAT clause
+        :param min_clause_length: minimum number of variables in a clause
+        :param max_clause_length: maximum number of variables in a clause
+    """
+    clause_length = np.random.randint(min_clause_length, max_clause_length + 1)
+    clause = []
+    available_variables = variables.copy()
+    for _ in range(clause_length):
+        variable = np.random.choice(available_variables)
+        is_not_negated = np.random.choice([True, False])
+        clause.append((variable, is_not_negated))
+        available_variables.remove(variable)
+    return clause
 
 
 class ProblemInstance:
@@ -110,8 +305,9 @@ class ProblemInstance:
 
     def __str__(self) -> str:
         return f"sat_instance: " + self.sat_instance_to_string() + "\n" \
-               f"n_features: {len(self.boolean_variables)}\n" \
-               f"feature_cost: {self.feature_cost}"
+                                                                   f"n_features: {len(self.boolean_variables)}\n" \
+                                                                   f"feature_cost: {self.feature_cost}\n" \
+                                                                   f"alpha_sat: {self.alpha_sat}"
 
     def sat_instance_to_string(self) -> str:
         instance_parts = []
@@ -130,6 +326,9 @@ class ProblemInstance:
 
     def get_num_features(self) -> int:
         return len(self.boolean_variables)
+
+    def get_boolean_variables(self) -> list[boolean_var]:
+        return self.boolean_variables
 
     def get_feature_cost(self) -> list[int]:
         return self.feature_cost
