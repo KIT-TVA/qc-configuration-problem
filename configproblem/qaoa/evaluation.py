6d5d7b4ac58273dbc911fc2ede63d4c852a2dd61
Fabian Brenneisen <fabian.brenneisen@student.kit.edu>
2023-11-07 17:16:31 +0100

    Change data saved in dataframe

--- configproblem/qaoa/evaluation.py
+++ configproblem/qaoa/evaluation.py
@@ -37,9 +37,6 @@ def run_puso_qaoa(instance: ProblemInstance, layers: int, strategy: str) -> dict
 
     return {'hamiltonian': hamiltonian,
             'probabilities': probabilities_dict,
-            'success_probability': instance.get_success_probability(probabilities_dict),
-            'best_config_probability': probabilities_dict[instance.get_best_config()],
-            'result_quality': instance.get_result_quality(probabilities_dict),
             'problem_circuit_depth': puso_problem_circuit(hamiltonian, get_hamiltonian_dimension(hamiltonian),
                                                           Parameter("$\\gamma$")).depth(),
             'circuit_width': get_hamiltonian_dimension(hamiltonian)}
@@ -68,9 +65,6 @@ def run_quso_qaoa(instance: ProblemInstance, layers: int, strategy: str) -> dict
 
     return {'hamiltonian': hamiltonian,
             'probabilities': probabilities_dict,
-            'success_probability': instance.get_success_probability(probabilities_dict),
-            'best_config_probability': probabilities_dict[instance.get_best_config()],
-            'result_quality': instance.get_result_quality(probabilities_dict),
             'problem_circuit_depth': puso_problem_circuit(hamiltonian, get_hamiltonian_dimension(hamiltonian),
                                                           Parameter("$\\gamma$")).depth(),
             'circuit_width': get_hamiltonian_dimension(hamiltonian)}
@@ -100,19 +94,16 @@ def run_instance(instance: ProblemInstance, layers: int, strategy: str) -> dict:
             'min_literals_per_clause': min_literals_per_clause,
             'max_literals_per_clause': max_literals_per_clause,
             'feature_cost': instance.get_feature_cost(),
-            'n_valid_configs': len(instance.get_valid_configs()),
+            'valid_configs': instance.get_valid_configs(),
+            'best_config': instance.get_best_config(),
             'n_layers': layers,
             'strategy': strategy,
-            'success_probability_puso': instance.get_success_probability(puso_results['probabilities']),
-            'best_config_probability_puso': puso_results['probabilities'][instance.get_best_config()],
-            'result_quality_puso': instance.get_result_quality(puso_results['probabilities']),
+            'probabilities_puso': puso_results['probabilities'],
             'problem_circuit_depth_puso': puso_problem_circuit(puso_results['hamiltonian'],
                                                                get_hamiltonian_dimension(puso_results['hamiltonian']),
                                                                Parameter("$\\gamma$")).depth(),
             'circuit_width_puso': get_hamiltonian_dimension(puso_results['hamiltonian']),
-            'success_probability_quso': instance.get_success_probability(quso_results['probabilities']),
-            'best_config_probability_quso': quso_results['probabilities'][instance.get_best_config()],
-            'result_quality_quso': instance.get_result_quality(quso_results['probabilities']),
+            'probabilities_quso': quso_results['probabilities'],
             'problem_circuit_depth_quso': puso_problem_circuit(quso_results['hamiltonian'],
                                                                get_hamiltonian_dimension(quso_results['hamiltonian']),
                                                                Parameter("$\\gamma$")).depth(),
@@ -147,41 +138,3 @@ def run_with_different_num_layers(instances: list[ProblemInstance], layers: list
         dataframes.append(run_instances_and_create_dataframe(instances, layer, strategy))
 
     return pd.concat(dataframes)
-
-
-def generate_instance_set_different_number_of_clauses(n_instances_per_n_clauses: int, n_features: int,
-                                                      n_clauses_list: list[int], n_literals_per_clause: int,
-                                                      min_feature_cost: int, max_feature_cost: int, alpha_sat: float,
-                                                      seed: int, generation_type: str) \
-        -> list[ProblemInstance]:
-    """
-        Generates a set of problem instances with different number of clauses
-
-        :param n_instances_per_n_clauses: The number of instances to generate
-        :param n_features: The number of features to use
-        :param n_clauses_list: The list of number of clauses to use
-        :param n_literals_per_clause: The number of literals per clause to use
-        :param min_feature_cost: The minimum feature cost to use
-        :param max_feature_cost: The maximum feature cost to use
-        :param alpha_sat: The alpha_sat to use
-        :param seed: The seed to use
-        :param generation_type: The generation type to use
-    """
-    instances = []
-    for n_clauses in n_clauses_list:
-        instances.extend(generate_problem_instance_set(n_instances_per_n_clauses, n_features, n_clauses, n_clauses,
-                                                       n_literals_per_clause, n_literals_per_clause, min_feature_cost,
-                                                       max_feature_cost, alpha_sat, seed, generation_type, None))
-    return instances
-
-
-def filter_out_unfulfillable_instances(instances: list[ProblemInstance]) -> list[ProblemInstance]:
-    """
-        Filters out unfulfillable instances from the given list of instances
-
-        :param instances: The instances to filter
-    """
-    for instance in instances:
-        if not instance.get_valid_configs():
-            instances.remove(instance)
-    return instances
